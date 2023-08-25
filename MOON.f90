6459f2f92497525afa35d86b2a60cf55a77a6197
DrK <norman.kirkby@gmail.com>
2023-08-25 16:22:32 +0100

    Added PLANET_data object

--- MOON.f90
+++ MOON.f90
@@ -3,8 +3,11 @@
 program MOON
 use, non_intrinsic :: PLANET
 implicit none
+type( PLANET_data ) :: earth
 
-write(*,*) 'Name = ' // PLANET_name()
+earth = PLANET_data( 'Earth', 6.e24 )
+
+write(*,*) 'Name = ' // earth % write()
 
 stop
 end program MOON
--- PLANET.f90
+++ PLANET.f90
@@ -3,11 +3,25 @@ implicit none; private
 
 public PLANET_name
 
+type, public :: PLANET_data
+    character(:), allocatable :: name 
+    real                      :: mass ! kg
+contains
+    procedure, pass :: write => PLANET_name
+end type PLANET_data
+
 contains
 
-function PLANET_name result(name)
-character(:), allocatable :: name
-name = "PLANET"
+function PLANET_name( d ) result( name )
+class( PLANET_data ), intent(in) :: d
+character(:), allocatable        :: name
+
+if( allocated( d % name ) ) then
+    name = d % name
+else
+    name = 'Name not allocated'
+end if
+
 end function PLANET_name
 
 end module PLANET
